3fabf97295b93366b1f7628ea1f2aa28a6d674ef
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-04-04 23:54:37 +0330

    FEAT: database model updated , ERR NOT FIXED

--- src/database/migrations/20240404083956_online_education_migrate/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Student" (
-    "id" UUID NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-
-    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
-);
